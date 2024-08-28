1272338be7599bea19b93ddf97a1b67b59ff5c7f
Danilo <damilhome@gmail.com>
2024-08-28 12:03:55 -0300

    atualizando profile.json e atualizando informações dinamicamente na pagina

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/projeto-base/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/damilhome/js-developer-portfolio/projeto-base/data/profile.json';
     const fetching = await fetch(url);
     return await fetching.json();
 }
